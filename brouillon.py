80b6dedeaaccc6a962aaf7d7f766104ead281ff7
sebilles <85125279+sebilles@users.noreply.github.com>
2022-11-08 16:34:36 +0100

    Update brouillon.py

--- brouillon.py
+++ brouillon.py
@@ -3,3 +3,10 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
+class Mouton():
+  positiion = np.array(2, dtype = float) #coords cartésiennes
+  vitesse = np.array(2, dtype = float) #coords polaires
+  
+  def update():
+    vit_cart = np.array([vitesse[0] * np.cos(vitesse[1]), vitesse[0] * np.sin(vitesse[1])])
+    position += vit_cart
